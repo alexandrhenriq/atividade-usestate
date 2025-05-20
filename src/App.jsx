a7a5d8076f214b7ec0daacfbe6b66033c214c784
alexandrehenriquegomeslopes <alexandrehenriquegomeslopes@gmail.com>
2025-05-20 03:11:53 +0000

    fim da atividade de useState, primeiro e ultimo commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import  { useState } from 'react'
+
+export function App() {
+  const [numero, setNumero] = useState(100)
+  const [mostrarEsconder, setMostrarEsconder] = useState(false)
+]
+  
+  function handleAcrescentar() {
+    setNumero(numero + 100)
+  }
+
+  function handleMostrarEsconderParagrafo() {
+    mostrarEsconder ? setMostrarEsconder(false) : setMostrarEsconder(true)
+  }
+  
+  return (
+    <div>
+      <h1>Atividade useState</h1>
+      <div>
+        <h2>Numero: {numero}</h2>
+
+        <button onClick={handleAcrescentar}>Acrescentar 100</button>
+      </div>
+      <div>
+        <h2>O paragrafo sumidouro</h2>
+
+        {mostrarEsconder ? <p>Este paragrafo sumidouro</p> : ""}
+
+        <button onClick={handleMostrarEsconderParagrafo}>{mostrarEsconder === true ? "Esconder" : "Mostrar"}</button>
+      </div>
+    </div>
+  )
+}
